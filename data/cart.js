6246ece428de635f68a0bdaa23a547c9ae5bbca4
hakankvrga <hakankvrga@gmail.com>
2025-12-19 20:04:25 +0300

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart=[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2
 },
@@ -9,7 +12,14 @@ export let cart = [{
 }
 
 ];
+}
 
+
+
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId, selectElement){
     let matchingItem; 
@@ -28,6 +38,8 @@ export function addToCart(productId, selectElement){
              quantity: Number(selectElement.value)
           });
           }
+
+          saveToStorage();
 }
 
 
@@ -41,4 +53,5 @@ export function removeFromCart(productId) {
   });
 
   cart =newCart;
+  saveToStorage();
 }
