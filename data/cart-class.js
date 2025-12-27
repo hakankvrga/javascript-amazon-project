759ecac7a88f706f044f12df46ed5f9de6397ddb
hakankvrga <hakankvrga@gmail.com>
2025-12-27 17:26:28 +0300

    Make localStorageKey and loadFromStorage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,16 +1,16 @@
 class Cart {
     cartItems;
-    localStorageKey;
+    #localStorageKey;
 
     constructor(localStorageKey){
-        this.localStorageKey= localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey= localStorageKey;
+        this.#loadFromStorage();
         
     }
 
-    loadFromStorage() {
+    #loadFromStorage() {
 
-        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+        this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
 
             if(!this.cartItems){
             this.cartItems=[{
@@ -29,7 +29,7 @@ class Cart {
     }
 
     saveToStorage(){
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+        localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
     }
 
 
