353bf01ff1bd6c41d4248e3b143b951f08e84400
hakankvrga <hakankvrga@gmail.com>
2025-12-29 23:43:40 +0300

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,14 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
+
 //import '../data/cart-class.js';
-import '../data/car.js'
-import '../data/backend-practise.js'
+//import '../data/car.js'
+//import '../data/backend-practise.js'
 
 
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
